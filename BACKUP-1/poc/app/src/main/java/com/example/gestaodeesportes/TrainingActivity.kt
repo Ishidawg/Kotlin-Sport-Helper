dd8bea6d6ac879b9c3a3bcd5aee1e96ab8d1e783
Willian <78714158+Ishidawg@users.noreply.github.com>
2024-10-26 01:12:54 -0300

    Backup: poc-1

---
+++ BACKUP-1/poc/app/src/main/java/com/example/gestaodeesportes/TrainingActivity.kt
@@ -0,0 +1,37 @@
+package com.example.gestaodeesportes
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+class TrainingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.training_screen)
+
+        val backToHome = findViewById<View>(R.id.backtohome)
+
+        val showless = findViewById<View>(R.id.showless)
+        val showmore = findViewById<View>(R.id.showmore)
+
+        val secondrow = findViewById<View>(R.id.secondrow)
+        val thirdrow = findViewById<View>(R.id.thirdrow)
+
+        backToHome.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        showless.setOnClickListener {
+            secondrow.visibility = View.GONE
+            thirdrow.visibility = View.GONE
+        }
+
+        showmore.setOnClickListener {
+            secondrow.visibility = View.VISIBLE
+            thirdrow.visibility = View.VISIBLE
+        }
+
+    }
+}
